9dec4902d7a1b50ea245a4c25b80a079f2575f9e
jinjor <jinjorweb@gmail.com>
2017-01-21 12:52:36 +0900

    remove elm from deploy process

--- build.sh
+++ build.sh
@@ -6,6 +6,3 @@ mkdir -p tmp
 cp ~/.local/bin/showit-compile tmp/showit-compile &&
 node scripts/copy-bins.js &&
 rm -rf tmp
-
-# make runtime and copy it
-elm-make runtime/Viewer.elm runtime/Editor.elm --output=lib/scaffold/showit-runtime.js
